74dedfd0c1fa18e150f92f60c22f910f16b738a7
Kenneth de Mello <k6demello28@fastmail.fm>
2020-08-10 14:31:49 -0400

    did the java file stuff

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,14 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		String numString = Long.toString(num);
+		long length = numString.length();
+		
+		long sum = 0;
+		for (int i = 0; i < numString.length(); i++) {
+			sum += Math.pow((Double)Long.parseLong(numString.charAt(i)), (Double)length);
+		}
+		
+		return sum == num;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,52 @@
 package com.revature.tier1;
 
 public class User {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String role;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String name) {
+		this.firstName = name;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String name) {
+		this.lastName = name;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String newpw) {
+		this.password = newpw;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
